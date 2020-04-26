caa0354fbe2a0103decf8e16a6bcbf8471ea1689
nbaugh1 <nbaugh1@gmail.com>
2020-04-26 11:46:50 -0400

    import react-bootstrap, work on using adding hooks to fetch data, use state

--- src/components/RechartsLineChart.js
+++ src/components/RechartsLineChart.js
@@ -1,41 +1,48 @@
-import React from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
-
-
+import React from "react";
+import useFetch from '../hooks'
+import {
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+} from "recharts";
+import Button from 'react-bootstrap/Button'
 
 const RechartsLineChart = () => {
-    const data = [
-        {
-            name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-        },
-        {
-            name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-            name: 'Page C', uv: 1000, pv: 9800, amt: 2290,
-        },
-        {
-            name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-            name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-            name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-        },
-        {
-            name: 'Page G', uv: 4000, pv: 4300, amt: 2100,
-        },
-    ]
-    return (
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-        </LineChart>
-    )
-}
 
-export default RechartsLineChart
+
+
+    // const req = async () => {
+    //     const arr = []
+    //     const resp = await fetch("https://pomber.github.io/covid19/timeseries.json")
+    //     const data = await resp.json()
+    //     const info = await data["US"].map(({ date, confirmed, recovered, deaths }) =>
+    //         arr.push(date, confirmed, recovered, deaths)
+    //     )
+    //     return arr
+    // }
+    // setData()
+
+
+    return (
+        <div>
+            <LineChart
+                LineChart
+                width={1000}
+                height={400}
+                data={["test"]}
+                margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            >
+                <Line type="monotone" dataKey="deaths" stroke="#8884d8" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+            <Button>Click</Button>
+        </div>
+    );
+};
+
+export default RechartsLineChart;
